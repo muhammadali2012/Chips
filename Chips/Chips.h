17bcb7af61fbefa0ecb7100753282696afa76bf0
Muhammad Ali <muhammad.ali@pakeventures.com>
2021-01-30 18:04:08 +0500

    Initial Commit

---
+++ Chips/Chips.h
@@ -0,0 +1,18 @@
+//
+//  Chips.h
+//  Chips
+//
+//  Created by Muhammad Ali on 30/01/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Chips.
+FOUNDATION_EXPORT double ChipsVersionNumber;
+
+//! Project version string for Chips.
+FOUNDATION_EXPORT const unsigned char ChipsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Chips/PublicHeader.h>
+
+
