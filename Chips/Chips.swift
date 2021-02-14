1feada3248bace58ae794463178242b486934fc2
muhammadali2012 <aliarham2012@gmail.com>
2021-02-14 17:22:38 +0500

    Add Test Cases

---
+++ Chips/Chips.swift
@@ -0,0 +1,19 @@
+//
+//  Chips.swift
+//  Chips
+//
+//  Created by Muhammad Ali on 14/02/2021.
+//
+
+import Foundation
+public final class Chips {
+    let name = "Chips"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
--- ChipsExampleTests/ChipsExampleTests.swift
+++ ChipsExampleTests/ChipsExampleTests.swift
@@ -1,33 +1,26 @@
 //
-//  ChipsTests.swift
-//  ChipsTests
+//  ChipsExampleTests.swift
+//  ChipsExampleTests
 //
-//  Created by Muhammad Ali on 30/01/2021.
+//  Created by Muhammad Ali on 14/02/2021.
 //
 
 import XCTest
-@testable import Chips
+@testable import ChipsExample
 
-class ChipsTests: XCTestCase {
-    var chips: Chips!
-
+class ChipsExampleTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        chips = Chips()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testAdd() throws {
+    func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(chips.add(a: 1, b: 1), 2)
-    }
-    func testSub() throws {
-        XCTAssertEqual(chips.sub(a: 1, b: 1), 0)
     }
 
     func testPerformanceExample() throws {
