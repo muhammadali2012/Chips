55a52eb73f4d88540aff696bea823a3857dcb880
muhammadali2012 <aliarham2012@gmail.com>
2021-03-14 12:43:41 +0500

    override error fix

--- ChipsTests/ChipsTests.swift
+++ ChipsTests/ChipsTests.swift
@@ -11,7 +11,7 @@ import XCTest
 class ChipsTests: XCTestCase {
     var chips: Chips!
 
-
+    /*
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         chips = Chips()
@@ -20,13 +20,15 @@ class ChipsTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    */
     func testAdd() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        chips = Chips()
         XCTAssertEqual(chips.add(a: 1, b: 1), 2)
     }
     func testSub() throws {
+        chips = Chips()
         XCTAssertEqual(chips.sub(a: 1, b: 1), 0)
     }
 
